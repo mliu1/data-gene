ccc8221c52836f40444aab3b885ff1c4a4ee1149
Ming Liu <mingliu@mings-mbp.attlocal.net>
2023-08-24 11:50:59 -0700

    wrap the code in main function and fetch_html_content function

--- download.py
+++ download.py
@@ -22,24 +22,30 @@ def save_html_to_file(html_content, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(html_content)
 
-parser = argparse.ArgumentParser(description="Download HTML content by hash.")
-parser.add_argument("--hash", type=str, help="The hash string used in the URL.")
-args = parser.parse_args()
+def fetch_html_content(url_base, hash_string):
+    url = url_base + hash_string
+    response = requests.get(url)
+    html_content = ""
+    if response.status_code == 200:
+        data = response.json()
+        content = data['html']
+        html_content = add_html_tag_if_missing(content)
+    else:
+        print("Failed to retrieve data. Status code:", response.status_code)
+    return html_content
 
-hash_string = args.hash
-url = 'https://form-fill-mongodb.vercel.app/api/html/find?hash='+hash_string
-response = requests.get(url)
-
-if response.status_code == 200:
-    data = response.json()
-    content = data['html']
-    html_content = add_html_tag_if_missing(content)
-    # File path where you want to save the HTML content
-    file_path = 'output.html'
-
-    # Save the HTML content to the file
-    save_html_to_file(html_content, file_path)
-else:
-    print("Failed to retrieve data. Status code:", response.status_code)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download HTML content by hash.")
+    parser.add_argument("--hash", type=str, help="The hash string used in the URL.")
+    args = parser.parse_args()
+    hash_string = args.hash
+    html_content = fetch_html_content('https://form-fill-mongodb.vercel.app/api/html/find?hash=', hash_string)
+    if len(html_content) > 0:
+        # File path where you want to save the HTML content
+        file_path = 'output.html'
+        # Save the HTML content to the file
+        save_html_to_file(html_content, file_path)
+    else:
+        print("Failed to retrieve data.")
 
 
