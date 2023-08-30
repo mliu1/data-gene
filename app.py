319ea1a1e443a7f76670b648e63fa45f35c7e0fe
Ming Liu <mingliu@mings-mbp.attlocal.net>
2023-08-29 21:22:50 -0700

    add parameter and fetch html

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import requests
+from lib_data_gene.download import fetch_html_content
+
+params = st.experimental_get_query_params()
+hash_string = params['hash']
+
+url = 'https://form-fill-mongodb.vercel.app/api/html/find?hash='
+html_content = fetch_html_content(url, hash_string)
+if (len(html_content) > 0):
+    print(html_content)
